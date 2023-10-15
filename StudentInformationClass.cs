d479b7c15a4c585ad66f22a1505ee01ff3c7fcb0
Rodney Charles Austria <austria.174768@balagtas.sti.edu.ph>
2023-10-15 12:15:45 +0800

    Add project files.

---
+++ StudentInformationClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03LaboratoryExercise_Austria
+{
+    class StudentInformationClass
+    {
+        public static int SetStudentNo, SetAge = 0;
+        public static string SetProgram, SetContactNo, SetGender, SetBirthday, SetFullName = " ";
+
+    }
+}
